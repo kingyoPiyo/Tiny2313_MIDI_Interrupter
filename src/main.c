331c3fb7316138c8ac6dca23fa7174bbcf553427
kingyo <null>
2019-01-15 00:53:56 +0900

    Revert "Update main.c"
    This reverts commit b7592b0bf6c73cfcea4b1417c811741caf1280c0.

--- src/main.c
+++ src/main.c
@@ -3,18 +3,18 @@
  *
  * 製作開始: 2013/05/03
  * 最終更新: 2013/07/07
- * 製作者 : kingyo
+ * 製作者 : 金魚
  *
  * クロック: 20MHz
  * 発音可能ノート番号: 0-108
  *
  * MIDIメッセージ(参考)
  * MIDI_NoteOff			(0x8x)
- * MIDI_NoteOn				(0x9x)
- * MIDI_ControlChange		(0xBx)	// 未使用
- * MIDI_ProgramChange		(0xCx)	// 未使用
+ * MIDI_NoteOn			(0x9x)
+ * MIDI_ControlChange	(0xBx)	// 未使用
+ * MIDI_ProgramChange	(0xCx)	// 未使用
  * MIDI_ChannelPressure	(0xDx)	// 未使用
- * MIDI_PitchBend			(0xEx)	// 未使用
+ * MIDI_PitchBend		(0xEx)	// 未使用
  */ 
 
 #include <avr/io.h>
@@ -24,21 +24,21 @@
 
 // 定義値
 #define BUFSIZE	16			// バッファサイズ 2^nバイト
-#define WAON		6			// 和音数(変更しないで！)
+#define WAON	6			// 和音数(変更しないで！)
 
 // グローバル変数
 uint8_t out = 0x07;			// out初期値
 uint8_t s_playing[WAON+1];	// 6和音 + 音源不足検知用
-uint8_t out_port[6];			// 出力ピン指定用
+uint8_t out_port[6];		// 出力ピン指定用
 uint8_t out_flg[3] = {0, 0, 0};
-uint8_t s_l, s_h;				// ノート番号で振り分ける際の閾値
+uint8_t s_l, s_h;			// ノート番号で振り分ける際の閾値
 uint16_t t_l[WAON];			// 音程に対応したパルス幅を格納
 uint16_t c,  num[WAON];		// タイマ割り込み内で使用
 PROGMEM const uint16_t table[] = {12740, 12025, 11350, 10713, 10111, 9544, 9008, 8503, 8025, 7575, 7150, 6748, 6369, 6012, 5674, 5356, 5055, 4771, 4504, 4251, 4012, 3787, 3574, 3374, 3184, 3005, 2837, 2677, 2527, 2385, 2251, 2125, 2006, 1893, 1787, 1686, 1592, 1502, 1418, 1338, 1263, 1192, 1125, 1062, 1002, 946, 893, 843, 795, 751, 708, 669, 631, 596, 562, 530, 501, 472, 446, 421, 397, 375, 354, 334, 315, 297, 281, 265, 250, 236, 222, 210, 198, 187, 176, 166, 157, 148, 140, 132, 124, 117, 111, 104, 99, 93, 88, 83, 78, 74, 69, 65, 62, 58, 55, 52, 49, 46, 43, 41, 39, 36, 34, 32, 30, 29, 27, 25, 24};
 
 // リングバッファ用変数
 uint8_t RxBuf[BUFSIZE];		// バッファ
-uint8_t Pdata;					// ポインタ
+uint8_t Pdata;				// ポインタ
 
 // delay関数(アバウト)
 void delay_ms(uint16_t time){
@@ -54,11 +54,11 @@ void sound_play(uint8_t note, uint8_t pin)
 {
 	uint8_t i;
 	
-	if(note >= 0 && note <= 108){					// 発音可能範囲のみ(0-108)
+	if(note >= 0 && note <= 108){				// 発音可能範囲のみ(0-108)
 		for(i=0; i<6; i++){						// 音源1から順に発音中か確認して発音していないならそれを使用する
-			if(s_playing[i] == 0x80){				// 発音していないか？
+			if(s_playing[i] == 0x80){			// 発音していないか？
 				t_l[i] = pgm_read_word(&table[note]);	// 音程に対応したパルス幅を格納
-				num[i] = c + t_l[i];				// 初期値計算
+				num[i] = c + t_l[i];			// 初期値計算
 				s_playing[i] = note;
 				switch(pin){
 					case 0: out_port[i] = 0b11111110; out_flg[0]++; break;
@@ -87,7 +87,7 @@ void sound_stop(uint8_t note, uint8_t pin)
 	
 	for(i=0; i<6; i++){							// 音源1から順にノートオフに当たる音源を探し、見つかれば発音停止
 		if(s_playing[i] == note && out_port[i] == prt){		// 探しているノート番号でかつポートも同じか？
-			s_playing[i] = 0x80;					// 無音に設定(0x80は無音)
+			s_playing[i] = 0x80;				// 無音に設定(0x80は無音)
 			switch(pin){
 				case 0: out_flg[0]--; break;
 				case 1: out_flg[1]--; break;
@@ -112,14 +112,14 @@ ISR(USART_RX_vect)
 // タイマ0比較一致A割り込み(9.6usごとに呼ばれる)
 ISR(TIMER0_COMPA_vect)
 {
-	PORTB = out;						// 出力ポート状態更新
-	c++;								// 16bit変数カウントアップ
+	PORTB = out;					// 出力ポート状態更新
+	c++;							// 16bit変数カウントアップ
 	out = 0xff;						// Hiに戻す
 	
 	if(s_playing[0] != 0x80){			// 音源1
 		if(c == num[0]){
 			num[0] += t_l[0];
-			out &= out_port[0];		// 対応ポートをLoにする
+			out &= out_port[0];			// 対応ポートをLoにする
 		}
 	}
 
@@ -174,10 +174,10 @@ void main(void)
 	uint8_t status, byte_1, byte_cnt, RxByte, Pout, mode, i, mode4_cnt = 0;
 	
 	// IOポート設定
-	DDRA = 0x00;				// ポートAは入力
-	DDRB = 0xff;				// ポートBは出力
+	DDRA = 0x00;			// ポートAは入力
+	DDRB = 0xff;			// ポートBは出力
 	DDRD = 0b01011100;		// ポートDの2,3,4,6だけ出力
-	PORTA = PORTD = 0;			// ポート状態初期化
+	PORTA = PORTD = 0;		// ポート状態初期化
 	PORTB = 0b00000111;
 	
 	for(i=0; i < WAON; i++){	// 配列初期化
@@ -188,7 +188,7 @@ void main(void)
 	// モード判別
 	switch(PIND & 0x22){
 		case 0x00: mode = 1; break;	// チャンネル区別再生
-		case 0x02: mode = 3; break;	// 設定ノート番号で別れる
+		case 0x02: mode = 3; break; // 設定ノート番号で別れる
 		case 0x20: mode = 2; break;	// チャンネルを区別せずに再生
 		case 0x22: mode = 4; PORTD = PIND | 0b01000000;	break;	// 機能設定モード(LED4を点灯させる)
 		default: mode = 0; break;
@@ -199,14 +199,14 @@ void main(void)
 	s_h = eeprom_read_byte(1);
 	
 	// タイマ0設定
-	TCCR0A = 0b00000010;		// CTC動作
-	TCCR0B = 0b00000011;		// Clk/64
-	OCR0A = 2;					// カウンタトップ値(割り込み周期 9.6us) [OCR0A = (9.6us/3.2us)-1 = 2]
+	TCCR0A = 0b00000010;	// CTC動作
+	TCCR0B = 0b00000011;	// Clk/64
+	OCR0A = 2;				// カウンタトップ値(割り込み周期 9.6us) [OCR0A = (9.6us/3.2us)-1 = 2]
 	TIMSK = 0b00000001;		// 比較A割り込み許可
 	
-	Pdata = Pout = 0;			// バッファ初期化
-	Usart_init();				// Usart初期化
-	sei();						// 全割り込み許可
+	Pdata = Pout = 0;		// バッファ初期化
+	Usart_init();			// Usart初期化
+	sei();					// 全割り込み許可
 	
 	///////////// メインループ //////////////
 	while(1) {
